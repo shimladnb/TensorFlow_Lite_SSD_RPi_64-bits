c1e4b07e1032d0fd6f68a161307436302cd13188
Sem Shimla <semschreuder@live.nl>
2024-12-22 07:47:47 +0100

    it sends dummy osc

--- MobileNetV1.cpp
+++ MobileNetV1.cpp
@@ -32,24 +32,50 @@ const size_t height = 300;
 std::vector<std::string> Labels;
 std::unique_ptr<tflite::Interpreter> interpreter;
 
+UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
+
+float testOsc[20];
+
+
+void SendOscMessage()
+{
+    for(int i : testOsc)
+    {
+        testOsc[i] = i;
+    }
+
+    char buffer[OUTPUT_BUFFER_SIZE];
+    osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+    p << osc::BeginBundleImmediate;
+    p << osc::BeginMessage( "/SkeletonPosition" );
+    for (int i=0; i<17; i++)
+    {
+        p << testOsc[i];
+    }
+    p << osc::EndMessage;
+    p << osc::EndBundle;
+
+    transmitSocket.Send( p.Data(), p.Size() );
+};
+
 static bool getFileContent(std::string fileName)
 {
 
-	// Open the File
-	std::ifstream in(fileName.c_str());
-	// Check if object is valid
-	if(!in.is_open()) return false;
+    // Open the File
+    std::ifstream in(fileName.c_str());
+    // Check if object is valid
+    if(!in.is_open()) return false;
 
-	std::string str;
-	// Read the next line from File untill it reaches the end.
-	while (std::getline(in, str))
-	{
-		// Line contains string of length > 0 then save it in vector
-		if(str.size()>0) Labels.push_back(str);
-	}
-	// Close The File
-	in.close();
-	return true;
+    std::string str;
+    // Read the next line from File untill it reaches the end.
+    while (std::getline(in, str))
+    {
+        // Line contains string of length > 0 then save it in vector
+        if(str.size()>0) Labels.push_back(str);
+    }
+    // Close The File
+    in.close();
+    return true;
 }
 
 void detect_from_video(Mat &src)
@@ -82,8 +108,10 @@ void detect_from_video(Mat &src)
 //        cout << "number of detections: " << num_detections << "\n";
 
     const float confidence_threshold = 0.5;
-    for(int i = 0; i < num_detections; i++){
-        if(detection_scores[i] > confidence_threshold){
+    for(int i = 0; i < num_detections; i++)
+    {
+        if(detection_scores[i] > confidence_threshold)
+        {
             int  det_index = (int)detection_classes[i]+1;
             float y1=detection_locations[4*i  ]*cam_height;
             float x1=detection_locations[4*i+1]*cam_width;
@@ -92,13 +120,15 @@ void detect_from_video(Mat &src)
 
             Rect rec((int)x1, (int)y1, (int)(x2 - x1), (int)(y2 - y1));
             rectangle(src,rec, Scalar(0, 0, 255), 1, 8, 0);
-            putText(src, format("%s", Labels[det_index].c_str()), Point(x1, y1-5) ,FONT_HERSHEY_SIMPLEX,0.5, Scalar(0, 0, 255), 1, 8, 0);
+            putText(src, format("%s", Labels[det_index].c_str()), Point(x1, y1-5),FONT_HERSHEY_SIMPLEX,0.5, Scalar(0, 0, 255), 1, 8, 0);
         }
     }
 }
 
 int main(int argc,char ** argv)
 {
+
+
     float f;
     float FPS[16];
     int i, Fcnt=0;
@@ -106,7 +136,7 @@ int main(int argc,char ** argv)
     const    char* gst = "libcamerasrc camera-name=/base/axi/pcie@120000/rp1/i2c@80000/ov5647@36 ! video/x-raw,width=1296,height=972,framerate=20/1,format=RGBx ! videoconvert ! videoscale !  video/x-raw,width=1296,height=972,format=BGR ! appsink";
     chrono::steady_clock::time_point Tbegin, Tend;
 
-    for(i=0;i<16;i++) FPS[i]=0.0;
+    for(i=0; i<16; i++) FPS[i]=0.0;
 
     // Load model
     std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile("detect.tflite");
@@ -117,38 +147,51 @@ int main(int argc,char ** argv)
 
     interpreter->AllocateTensors();
 
-	// Get the names
-	bool result = getFileContent("COCO_labels.txt");
-	if(!result)
-	{
+    // Get the names
+    bool result = getFileContent("COCO_labels.txt");
+    if(!result)
+    {
         cout << "loading labels failed";
         exit(-1);
-	}
+    }
 
     VideoCapture cap(gst);
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         cerr << "ERROR: Unable to open the camera" << endl;
         return 0;
     }
 
     cout << "Start grabbing, press ESC on Live window to terminate" << endl;
-	while(1){
+    while(1)
+    {
+
 //        frame=imread("Traffic.jpg");  //need to refresh frame before dnn class detection
         cap >> frame;
-        if (frame.empty()) {
+        if (frame.empty())
+        {
             cerr << "ERROR: Unable to grab from the camera" << endl;
             break;
         }
+
+        SendOscMessage();
 
         Tbegin = chrono::steady_clock::now();
 
         detect_from_video(frame);
 
+        //--------------OSC
+
+        //--------------OSC
+
         Tend = chrono::steady_clock::now();
         //calculate frame rate
         f = chrono::duration_cast <chrono::milliseconds> (Tend - Tbegin).count();
         if(f>0.0) FPS[((Fcnt++)&0x0F)]=1000.0/f;
-        for(f=0.0, i=0;i<16;i++){ f+=FPS[i]; }
+        for(f=0.0, i=0; i<16; i++)
+        {
+            f+=FPS[i];
+        }
         putText(frame, format("FPS %0.2f", f/16),Point(10,20),FONT_HERSHEY_SIMPLEX,0.6, Scalar(0, 0, 255));
 
         //show output
@@ -159,9 +202,9 @@ int main(int argc,char ** argv)
         if(esc == 27) break;
     }
 
-  cout << "Closing the camera" << endl;
-  destroyAllWindows();
-  cout << "Bye!" << endl;
+    cout << "Closing the camera" << endl;
+    destroyAllWindows();
+    cout << "Bye!" << endl;
 
-  return 0;
+    return 0;
 }
